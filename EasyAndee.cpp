38fc826ba2db32a494419f11eb165e026e62fe1f
Hasif <hasif@annikken.com>
2018-08-08 16:35:51 +0800

    minor text edits

--- EasyAndee.cpp
+++ EasyAndee.cpp
@@ -4,7 +4,7 @@ Author: Muhammad Hasif
 
 The Easy Andee Library is to allow users to use a feature similar to Arduino's Firmata to control their Arduino using their Bluetooth enabled phone. Users no longer need to code the UI and logic. With the Easy Andee feature on the Andee App, users can create their UI and immediately control their electronics.
 
-This library is free software */
+This library is free software*/
 
 #if (ARDUINO >= 100)
 #include <Arduino.h>
